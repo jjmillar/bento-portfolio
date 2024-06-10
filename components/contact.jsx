2945b77e86a1adabcaf170eaa104425db7d71af4
Julián Millar <julianjmillar@gmail.com>
2024-06-10 15:53:33 -0400

    avance

--- components/contact.jsx
+++ components/contact.jsx
@@ -5,11 +5,11 @@ export default function Contact () {
   return (
     <div className='contact__wrapper'>
       <a href='https://github.com/jjmillar' className='scaleOnHover' target='blank'><Icon path={mdiGithub} title='githublink' /></a>
-      <a href=''><Icon path={mdiEmailOutline} /></a>
-      <a href=''><Icon path={mdiLinkedin} /></a>
-      <a href=''><Icon path={mdiCellphone} /></a>
-      <a href=''><Icon path={mdiMapMarkerOutline} /></a>
-      <a href=''><Icon path={mdiWhatsapp} /></a>
+      <a href='mailto:julianjmillar@gmail.com' className='scaleOnHover' target='blank'><Icon path={mdiEmailOutline} /></a>
+      <a href='https://www.linkedin.com/in/juli%C3%A1n-millar-vollaire/' className='scaleOnHover' target='blank'><Icon path={mdiLinkedin} /></a>
+      <a href='tel:+56972151199' className='scaleOnHover' target='blank'><Icon path={mdiCellphone} /></a>
+      <a href='https://maps.app.goo.gl/JgovNRqWAbMXMuQy8' className='scaleOnHover' target='blank'><Icon path={mdiMapMarkerOutline} /></a>
+      <a href='https://api.whatsapp.com/send/?phone=56972151199' className='scaleOnHover' target='blank'><Icon path={mdiWhatsapp} /></a>
     </div>
   )
 }
--- components/skills.jsx
+++ components/skills.jsx
@@ -3,18 +3,18 @@ import './components.scss'
 export default function Skills () {
   return (
     <div className='skills__wrapper'>
-      <img src='html5.svg' alt='html5 logo' />
-      <img src='css3.svg' alt='css3 logo' />
-      <img src='sass.svg' alt='sass logo' />
-      <img src='javascript.svg' alt='javascript logo' />
-      <img src='react.svg' alt='react logo' />
-      <img src='nextjs.svg' alt='nextjs logo' />
-      <img src='nodejs.svg' alt='nodejs logo' />
-      <img src='express.svg' alt='express logo' />
-      <img src='mongodb.svg' alt='mongodb logo' />
-      <img src='mysql.svg' alt='mysql logo' />
-      <img src='git.svg' alt='git logo' />
-      <img src='github.svg' alt='github logo' />
+      <img className='scaleOnHover' src='html5.svg' alt='html5 logo' />
+      <img className='scaleOnHover' src='css3.svg' alt='css3 logo' />
+      <img className='scaleOnHover' src='sass.svg' alt='sass logo' />
+      <img className='scaleOnHover' src='javascript.svg' alt='javascript logo' />
+      <img className='scaleOnHover' src='react.svg' alt='react logo' />
+      <img className='scaleOnHover' src='nextjs.svg' alt='nextjs logo' />
+      <img className='scaleOnHover' src='nodejs.svg' alt='nodejs logo' />
+      <img className='scaleOnHover' src='express.svg' alt='express logo' />
+      <img className='scaleOnHover' src='mongodb.svg' alt='mongodb logo' />
+      <img className='scaleOnHover' src='mysql.svg' alt='mysql logo' />
+      <img className='scaleOnHover' src='git.svg' alt='git logo' />
+      <img className='scaleOnHover' src='github.svg' alt='github logo' />
     </div>
   )
 }
