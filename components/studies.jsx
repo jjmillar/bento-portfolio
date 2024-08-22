dd50610d4237dbc4a8093e94a91bf2f786041f7b
Julián Millar <julianjmillar@gmail.com>
2024-08-22 11:31:21 -0400

    feat: add studies animation

--- components/studies.jsx
+++ components/studies.jsx
@@ -3,11 +3,11 @@ import './components.scss'
 export default function Studies () {
   return (
     <div className='studies__wrapper mask--image'>
-      <button className='studies__card cardOnHover'>FullStack MERN M.I.T. xPRO</button>
-      <button className='studies__card cardOnHover'>FullStack MERN Microsoft Launch-X Latam</button>
-      <button className='studies__card cardOnHover'>Udemy</button>
-      <button className='studies__card cardOnHover'>Inglés</button>
-      <button className='studies__card cardOnHover'>Ingeniería en Construcción</button>
+      <button className='studies__card studies1 cardOnHover'>FullStack MERN M.I.T. xPRO</button>
+      <button className='studies__card studies2 cardOnHover'>FullStack MERN Microsoft Launch-X Latam</button>
+      <button className='studies__card studies3 cardOnHover'>Udemy</button>
+      <button className='studies__card studies4 cardOnHover'>Inglés</button>
+      <button className='studies__card studies5 cardOnHover'>Ingeniería en Construcción</button>
     </div>
   )
 }
